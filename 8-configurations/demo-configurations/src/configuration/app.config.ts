349436159393c61b0c47a5f540dd450a4b56b729
Julien Prugne <julien@webeleon.dev>
2024-04-12 17:33:45 +0200

    feat: add config validation

--- 8-configurations/demo-configurations/src/configuration/app.config.ts
+++ 8-configurations/demo-configurations/src/configuration/app.config.ts
@@ -1,11 +1,26 @@
 import { ConfigType, registerAs } from '@nestjs/config';
-import * as process from 'process';
+import * as Joi from 'joi';
+
+const schema = Joi.object({
+  env: Joi.string().valid(...['dev', 'staging', 'production', 'test']),
+  port: Joi.number().port(),
+});
 
 export const appConfig = registerAs('app', () => {
-  return {
+  const config = {
     env: process.env.NODE_ENV ?? 'dev',
     port: !Number.isNaN(process.env.PORT) ? parseInt(process.env.PORT) : 3000,
   };
+
+  const validation = schema.validate(config, {
+    abortEarly: false,
+  });
+
+  if (validation.error) {
+    throw new Error(`invalid config: ${validation.error.message}`);
+  }
+
+  return config;
 });
 
 export type AppConfig = ConfigType<typeof appConfig>;
